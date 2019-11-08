6c85bc5b692a09686b18b69c33f643b1877e99fc
matyjb <matyjasiakbartosz@gmail.com>
2019-11-08 15:54:32 +0100

    zadanie 1

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,9 +15,46 @@ class MyApp extends StatelessWidget {
         ),
         body: Container(
           child: Center(
-            child: Text("Hello World!"),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(5),
+                child: Container(
+                color: Colors.grey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Text("Silver"),
+                            Text(".NET", style: TextStyle(color: Colors.red),),
+                          ],
+                        ),
+                        Text(
+                          "Flutter jest spoko",
+                          style: TextStyle(color: Colors.red, fontSize: 25),
+                        )
+                      ],
+                    ),
+                    Image.asset("assets/silver.png"),
+                    RaisedButton(
+                      onPressed: () {},
+                      color: Colors.red,
+                      child: Text("button, ktory nic nie robi"),
+                    )
+                  ],
+                ),
+                ),
+              ),
+              
+            ]
           ),
         ),
+      ),
       ),
     );
   }
