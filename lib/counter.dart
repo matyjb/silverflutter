6d9a5708f40f39b419eb238f644a401de93cf6e0
matyjb <matyjasiakbartosz@gmail.com>
2019-11-22 15:19:38 +0100

    wyciaganie danych z bloc

--- lib/counter.dart
+++ lib/counter.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:silverflutter/bloc/counter_bloc.dart';
 
 class Counter extends StatelessWidget {
   const Counter({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          IconButton(icon: Icon(Icons.add_circle),onPressed: (){},),
-          IconButton(icon: Icon(Icons.add),onPressed: (){},),
-          Text("0"),
-          IconButton(icon: Icon(Icons.remove),onPressed: (){},),
-          IconButton(icon: Icon(Icons.remove_circle),onPressed: (){},),
-        ],
+    return BlocBuilder<CounterBloc,CounterState>(
+      builder: (context, state) => Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(icon: Icon(Icons.add_circle),onPressed: (){},),
+            IconButton(icon: Icon(Icons.add),onPressed: (){},),
+            Text(state.value.toString()),
+            IconButton(icon: Icon(Icons.remove),onPressed: (){},),
+            IconButton(icon: Icon(Icons.remove_circle),onPressed: (){},),
+          ],
+        ),
       ),
     );
   }
