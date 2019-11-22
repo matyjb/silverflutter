d607ad29ac48cab43736d3a0a11549dc37503f04
matyjb <matyjasiakbartosz@gmail.com>
2019-11-22 15:14:27 +0100

    wrap in bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:silverflutter/bloc/counter_bloc.dart';
 import 'package:silverflutter/counter.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
@@ -7,15 +9,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Pierwsze zajęcia!"),
+    return BlocProvider<CounterBloc>(
+      builder: (context) => CounterBloc(),
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.red,
         ),
-        body: Counter(),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Pierwsze zajęcia!"),
+          ),
+          body: Counter(),
+        ),
       ),
     );
   }
